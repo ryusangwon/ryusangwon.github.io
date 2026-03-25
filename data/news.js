b96fe61967f88ba35bfaf229b9c3ba72d13bb153
ryusangwon <r8uuuuuuuu@gmail.com>
2026-03-25 13:13:07 +0100

    updates

--- data/news.js
+++ data/news.js
@@ -2,7 +2,7 @@ window.NEWS = [
   { date: "Mar 2026", text: "🎤 I will serve as a Session Chair for the HCI and Dialogue session at EACL 2026." },
   { date: "Mar 2026", text: "🎉 One paper has been accepted at Expert Systems with Applications." },
   { date: "Mar 2026", text: "👋 I have joined LMU Munich as a visiting researcher." },
-  { date: "Jan 2026", text: "🎉 One paper has been accepted at EACL 2026 Findings." },
+  { date: "Jan 2026", text: "🎉 One paper has been accepted at EACL 2026 (Findings)." },
   { date: "Sep 2025", text: "🎉 One paper has been accepted at the NLLP Workshop at EMNLP 2025." },
   { date: "Jun 2025", text: "🎊 I have been selected for the Samsung Electronics MX Scholarship." },
   { date: "May 2025", text: "🎉 One paper has been accepted at ACL 2025." },
